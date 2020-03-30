a63f6a98f1c7fd6e53bb77cbaadb331c286e5a5d
doohinkus <doohinkus@gmail.com>
2020-03-29 18:31:35 -0700

    complete app

--- src/components/Loot.js
+++ src/components/Loot.js
@@ -2,23 +2,30 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { fetchBitCoinFromApi } from '../redux/async'
 
-export function Loot({ bitcoin, fetchBitCoinFromApi }){
+export function Loot({ bitcoin, fetchBitCoinFromApi, balance }){
   useEffect(() => {
     fetchBitCoinFromApi();
     // console.log("fetch")
   });
-  // function calculateExchange(bitcoin, rate){
-  //   return rate * bitcoin;
-  // }
+  function calculateExchange(balance, rate){
+    // console.log(" >>>", balance, " ", rate, " ", balance/parseInt(rate.replace(",", "")))
+    // 1 -> $3.00 
+    // 1/3 x/6
+    // $6.00 -> 2
+    return 1/parseInt(rate.toString().replace(",", "")) * balance;
+  }
   return (
     <div>
-      <h3>Bitcoin Balance: {bitcoin}</h3>
+      <h3>USD: {balance}</h3>
+      <p>Bitcoin: {calculateExchange(balance,bitcoin)} </p>
     </div>
   )
 } 
 function mapStateToProps(state){
   return {
-    bitcoin: state.bitcoin
+    balance: state.balance,
+    bitcoin: state.bitcoin,
+
   }
 }
 
